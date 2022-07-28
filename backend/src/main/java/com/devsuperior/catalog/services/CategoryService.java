90958ee31d03df2b9fd58700652c3e837accc2fe
William Noel <noelwilliam2018@hotmail.com>
2022-07-27 21:39:56 -0300

    Find category by id, exception handling

--- backend/src/main/java/com/devsuperior/catalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/catalog/services/CategoryService.java
@@ -1,14 +1,18 @@
 package com.devsuperior.catalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.catalog.dto.CategoryDTO;
 import com.devsuperior.catalog.entites.Category;
 import com.devsuperior.catalog.repositories.CategoryRepository;
+import com.devsuperior.catalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -16,8 +20,17 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll(){
 		List<Category>list = repository.findAll();
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id){
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Categoria não encontrada!"));
+		return new CategoryDTO(entity);
+				
 	}
 }
---
+++ backend/src/main/java/com/devsuperior/catalog/services/exceptions/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.devsuperior.catalog.services.exceptions;
+
+public class EntityNotFoundException extends RuntimeException{
+	
+	public EntityNotFoundException(String msg) {
+		super(msg);	
+	}
+
+}
